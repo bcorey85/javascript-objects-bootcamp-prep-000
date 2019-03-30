e8d9033016de3b54bb03d3831bbed1b27221fb33
brandon corey <bcorey85@gmail.com>
2019-03-30 15:49:20 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,10 +1,19 @@
-var playlist = {artist: 'song'}
+// var playlist = {artist: 'song'}
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, {[artistName]: songTitle})
 }
 
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist[artistName]
-  return(playlist)
+// function removeFromPlaylist(playlist, artistName) {
+//   delete playlist[artistName]
+//   return(playlist)
+// }
+
+
+var playlist = {artistName: 'songTitle'}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  return Object.assign({}, {[artistName]:songTitle})
 }
+
+
